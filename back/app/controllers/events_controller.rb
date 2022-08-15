84a4712a8cc874b430e3a3c193b9c22c8396d6c6
mitchell <rksas927@gmail.com>
2022-08-15 21:39:42 +0900

    #11 rubocopの設定変更と指摘箇所修正

--- back/app/controllers/events_controller.rb
+++ back/app/controllers/events_controller.rb
@@ -1,19 +1,17 @@
+# This class is hot air balloon competition class.
 class EventsController < ApplicationController
   before_action :set_event, only: [:show, :update, :destroy]
 
-  # GET /events
   def index
     @events = Event.all
 
     render json: @events
   end
 
-  # GET /events/1
   def show
     render json: @event
   end
 
-  # POST /events
   def create
     @event = Event.new(event_params)
 
@@ -24,7 +22,6 @@ class EventsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /events/1
   def update
     if @event.update(event_params)
       render json: @event
@@ -33,19 +30,17 @@ class EventsController < ApplicationController
     end
   end
 
-  # DELETE /events/1
   def destroy
     @event.destroy
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_event
-      @event = Event.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def event_params
-      params.require(:event).permit(:name, :director)
-    end
+  def set_event
+    @event = Event.find(params[:id])
+  end
+
+  def event_params
+    params.require(:event).permit(:name, :director)
+  end
 end
