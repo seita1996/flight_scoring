2d6e59d42f96e88d2206ede9943097d0a1492046
mitchell <rksas927@gmail.com>
2022-08-26 22:40:34 +0900

    #40 create task type page.

--- back/app/controllers/task_types_controller.rb
+++ back/app/controllers/task_types_controller.rb
@@ -1,7 +1,41 @@
 class TaskTypesController < ApplicationController
+  before_action :set_task_type, only: %i[update destroy]
+
   def index
     @task_types = TaskType.all
 
     render json: @task_types
+  end
+
+  def create
+    @task_types = TaskType.new(task_type_params)
+
+    if @task_types.save
+      render json: @task_types, status: :created, location: @task_types
+    else
+      render json: @task_types.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @task_types.update(task_type_params)
+      render json: @task_types
+    else
+      render json: @task_types.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @task_types.destroy
+  end
+
+  private
+
+  def set_task_type
+    @task_types = TaskType.find(params[:id])
+  end
+
+  def task_type_params
+    params.require(:task_type).permit(:name)
   end
 end
--- front/pages/task_types/index.vue
+++ front/pages/task_types/index.vue
@@ -1,8 +1,45 @@
 <template lang="pug">
-v-main
-  div タスク一覧
+v-container
+  v-row(align="center" justify="center")
+    v-col(cols="12")
+      v-text-field(label="タスク名" v-model="name" prepend-icon="" type="text")
+      v-btn(color="primary" @click="createTaskType") 登録
+    v-col(cols="12")
+        h1 タスク一覧
+  v-card(class="mx-auto" max-width="300" tile)
+    v-list(rounded)
+      v-subheader TASKTYPE
+      v-list-item-group(color="primary")
+        v-list-item(v-for="task_type in task_types" :key="task_type.id" @click="")
+          v-list-item-content
+            v-list-item-title(v-text="task_type.name")
 </template>
 
 <script>
-export default {}
+import axios from '~/plugins/axios'
+
+export default {
+  data () {
+    return {
+      name: '',
+      task_types: []
+    }
+  },
+  created () {
+    axios.get('/task_types').then((res) => {
+      if (res.data) {
+        this.task_types = res.data
+      }
+    })
+  },
+  methods: {
+    createTaskType () {
+      axios.post('/task_types', { name: this.name }).then((res) => {
+        if (res.data) {
+          this.task_types.push(res.data)
+        }
+      })
+    }
+  }
+}
 </script>
