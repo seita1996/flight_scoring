d1d0057e3278ab5accfd743615c14238310ee27d
mitchell <rksas927@gmail.com>
2022-08-27 23:30:57 +0900

    #44 Modify term cloumn type of event table

--- back/db/schema.rb
+++ back/db/schema.rb
@@ -14,8 +14,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_22_145512) do
   create_table "events", charset: "utf8mb4", force: :cascade do |t|
     t.string "name", null: false
     t.string "director"
-    t.date "start_term"
-    t.date "end_term"
+    t.datetime "start_term", precision: nil
+    t.datetime "end_term", precision: nil
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
