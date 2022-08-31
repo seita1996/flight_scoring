ddc5d34b96453e4477e89087469b3c241a49d409
mitchell <rksas927@gmail.com>
2022-08-31 23:49:15 +0900

    #56 Created id page and function that move to that page.

---
+++ front/pages/events/_id/index.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+div _id/index.vueです
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+  created () {
+    console.log('this.$route.params')
+    console.log(this.$route.params)
+  },
+  methods: {
+  }
+}
+</script>
