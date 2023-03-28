2d99e00e8858fe5eb38ea119e7cc79d6b5b720e3
mitchell <rksas927@gmail.com>
2023-03-28 21:43:53 +0900

    微修正

--- front/pages/events/_id/index.vue
+++ front/pages/events/_id/index.vue
@@ -1,5 +1,14 @@
 <template lang="pug">
-div _id/index.vueです
+v-container
+  v-row(align="center" justify="center")
+    v-col(cols="12")
+      v-text-field(label="大会名" v-model="name" prepend-icon="" type="text")
+  v-row(align="center" justify="center")
+    v-col(cols="12")
+      v-text-field(label="イベントディレクター名" v-model="director" prepend-icon="" type="text")
+  DateRangePicker(@updateDates="updateDates")
+  v-btn(@click="moveEvent") 戻る
+  v-btn.pull-right(color="primary" @click="createEvent") 大会登録
 </template>
 
 <script>
@@ -13,6 +22,9 @@ export default {
     console.log(this.$route.params)
   },
   methods: {
+    moveEvent () {
+      this.$router.push('/events')
+    }
   }
 }
 </script>
