2624af785a931a0003d58bf1e89936c94f42042d
mitchell <rksas927@gmail.com>
2023-03-31 12:01:02 +0900

    #60 地図の中心座標を画面表示するようにした

--- front/components/MapArea.vue
+++ front/components/MapArea.vue
@@ -1,5 +1,10 @@
 <template lang="pug">
-v-sheet(id="map" class="pa-0" :light="true" style="width: 100%; height: 500px;")
+div
+  .box30
+    .box-title 中心地の緯度経度
+    p 緯度：{{ lat }}
+    p 経度：{{ lng }}
+  v-sheet(id="map" class="pa-0" :light="true" style="width: 100%; height: 500px;")
 </template>
 
 <script>
@@ -10,7 +15,9 @@ export default {
   data: function () {
     return {
       map: null,
-      google: null
+      google: null,
+      lat: 0,
+      lng: 0
     }
   },
   mounted: function () {
@@ -44,11 +51,39 @@ export default {
         // 3dビューを有効にする
         mapTypeId: 'satellite'
       })
-      // こちらにレスポンスとして受け取ったgoogleやthis.mapを使用すれば、
-      // 通常通りvueでもJavaScriptAPIを利用できます
+      // 緯度経度を初期化
+      this.lat = this.map.getCenter().lat()
+      this.lng = this.map.getCenter().lng()
+
+      // 常にマップの中心を画面表示
+      this.map.addListener('center_changed', () => {
+        this.lat = this.map.getCenter().lat()
+        this.lng = this.map.getCenter().lng()
+      })
     }).catch((e) => {
       console.error(e)
     })
   }
 }
 </script>
+
+<style>
+.box30 {
+    margin: 2em 0;
+    background: #f1f1f1;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.22);
+}
+.box30 .box-title {
+    font-size: 1.2em;
+    background: #5fc2f5;
+    padding: 4px;
+    text-align: center;
+    color: #FFF;
+    font-weight: bold;
+    letter-spacing: 0.05em;
+}
+.box30 p {
+    padding: 15px 20px;
+    margin: 0;
+}
+</style>
