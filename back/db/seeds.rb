d522e509ae912055ed4ac1ca62fd9d33bfecb20c
mitchell <rksas927@gmail.com>
2023-04-06 23:59:29 +0900

    #82 初期データをseedファイルに追加

--- back/db/seeds.rb
+++ back/db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "csv"
+
+# task_types
+unless TaskType.exists?
+  CSV.foreach('db/seeds/csv/task_type.csv', headers: true) do |task|
+    TaskType.create(
+      name: task['name'],
+      short_name: task['short_name'],
+      description: task['description']
+    )
+  end
+end
